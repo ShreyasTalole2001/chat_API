c417bda2e695054d0b64b734c2aa00b3d1542383
shreyash talole <shreyastalole2001@gmail.com>
2023-04-21 16:52:32 +0530

    Cors Enabled

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 require('./DB/mongoDB')
 
 var chatRouter = require('./routes/chat');
 
-var port = process.env.PORT || '3000';
+var port = process.env.PORT || '4000';
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 
 app.use('/chat', chatRouter);
--- routes/chat.js
+++ routes/chat.js
@@ -16,6 +16,11 @@ router.post('/createChat', async (req, res) => {
 
     try {
         await chat.save()
+        res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     } catch (error) {
         console.log(error)
         res.status(400).send(error)
